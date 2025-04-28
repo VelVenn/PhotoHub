8c084a8183e4e62ff413fa12a65cb374d87d4d01
LORAINE <162586864+VelVenn@users.noreply.github.com>
2025-04-28 12:37:59 +0800

    Test resources added

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -8,7 +8,13 @@ module io.loraine.photohub {
     requires org.kordamp.ikonli.materialdesign2;
     requires org.kordamp.ikonli.material2;
     requires eu.hansolo.tilesfx;
+    requires com.jfoenix;
+    requires java.desktop;
+    requires javafx.swing;
 
     opens io.loraine.photohub to javafx.fxml;
+    opens io.loraine.photohub.viewer to javafx.fxml;
+
     exports io.loraine.photohub;
+    exports io.loraine.photohub.viewer;
 }
